749f3b421dec7c8602cf2ac952ffe0637e367b7d
fvntr <phiven@gmail.com>
2015-07-21 22:06:02 -0700

    Added utils file

--- server/basic-server.js
+++ server/basic-server.js
@@ -1,23 +1,22 @@
 var http = require("http");
 var port = 3000;
 var ip = "127.0.0.1";
+var urlParser = require('url');
+var utils = require('./utils');
 
 var routes = {
 	'/classes/chatterbox': require('./request-handler')
 };
-
 var server = http.createServer(function(request, response){
 	console.log("Serving request type " + request.method + " for url " + request.url);
 	var parts = urlParser.parse(request.url);
-	
 	var route = routes[parts.pathname];
 	if(route){
 		route(request, response);
 	} else{
-		// 404
+		utils.sendResponse(response, 'Not Found, 404');
 	}
 });
-
 console.log("Listening on http://" + ip + ":" + port);
 server.listen(port, ip);
 
--- server/request-handler.js
+++ server/request-handler.js
@@ -1,47 +1,38 @@
-var headers = {
-  "access-control-allow-origin": "*",
-  "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
-  "access-control-allow-headers": "content-type, accept",
-  "access-control-max-age": 10,
-  'Content-Type' : "application/json"
-};
-
-var sendResponse = function(response, data, statusCode){
-  statusCode = statusCode || 200;
-  response.writeHead(statusCode, headers);
-  response.end(JSON.stringify(data));  
-};
-
-// This collects the data
-var collectData = function(request, callback){
-  var data = "";
-  request.on('data', function(chunk){
-    data += chunk;
-  });
-  request.on('end', function(){
-    callback(JSON.parse(data));
-  });
-};
-
+var utils = require('./utils');
+var objectId = 1;
 var messages = [
   {
     text:'goodbye world',
-    username: 'imgonnadoit'
+    username: 'imgonnadoit',
+    objectId: objectId
   }
 ];
-module.exports = function(request, response) {
-  var statusCode = 200;
 
-  if(request.method === "GET"){
-    sendResponse(response, {results: messages});
-  } else if (request.method === "POST"){
-    collectData(request, function(message){
+var actions = {
+  'GET': function(request, response){
+    utils.sendResponse(response, {results: messages});
+  },
+
+  'POST': function(request, response){
+    utils.collectData(request, function(message){
       message.objectId = ++objectId;
       messages.push(message);
-      sendResponse(response, {objectId: 1});
+      sendResponse(response, {objectId: objectId});
     });
-  } else if (request.method === "OPTIONS"){
-    sendResponse(response);
+  },
+
+  OPTIONS: function(request, response){
+    utils.sendResponse(response);
+  }
+};
+
+module.exports = function(request, response) {
+  var statusCode = 200;
+  var action = actions[request.method];
+  if(action){
+    action(request, response);
+  } else {
+    utils.sendResponse(response, 'Not Found, 404');
   }
 };
 
